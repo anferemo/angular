89b385c2e66eeb3abc05329a34404fca7d2a38ac
Andres Felipe Reyes Montes <anreyes@personalsoft.com.co>
2019-07-12 17:12:48 -0500

    commit inicial curso angular

---
+++ angular/clientes-app/src/app/clientes/clientes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.component.html',
+  styleUrls: ['./clientes.component.css']
+})
+export class ClientesComponent implements OnInit {
+
+  clientes: Cliente[] = [
+    {id: 1, nombre:'Sandra', apellido:'Mendieta',email:'sandra@mail.com.co', createAt:'2019-05-30'},
+    {id: 2, nombre:'Maritza', apellido:'Silva',email:'maritza78@mail.com.co', createAt:'2019-05-29'},
+    {id: 3, nombre:'Natalia', apellido:'Ramirez',email:'natis@hotmail.com', createAt:'2019-05-15'},
+    {id: 4, nombre:'Diana', apellido:'Silva',email:'diana@mail.com.co', createAt:'2019-05-12'},
+    {id: 5, nombre:'Paola', apellido:'Perez',email:'paola.perez@mail.com.co', createAt:'2019-05-16'},
+    {id: 6, nombre:'Juan', apellido:'Marin',email:'juan@mail.com.co', createAt:'2019-05-01'},
+    {id: 7, nombre:'Carlos', apellido:'Rincon',email:'carlosmar@gmail.com', createAt:'2019-05-28'},
+    {id: 8, nombre:'Monica', apellido:'Naranjo',email:'naram@mail.com.co', createAt:'2019-05-20'},
+    {id: 9, nombre:'Dahiana', apellido:'Lopez',email:'dalopez@mail.com.co', createAt:'2019-05-05'},
+    {id: 10, nombre:'Astrid', apellido:'Pereira',email:'asper@mail.com.co', createAt:'2019-05-23'}
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ angular/clientes-app/src/app/directiva/directiva.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html',
+  styleUrls: ['./directiva.component.css']
+})
+export class DirectivaComponent  {
+  listaArreglo: string[] = ['TypeScript', 'Javascript', 'Java SE', 'C#', 'PHP'];
+  habilitar: boolean = true;
+  constructor() { }
+
+  setHabilitar(): void {
+    this.habilitar = !this.habilitar;
+  }
+}
