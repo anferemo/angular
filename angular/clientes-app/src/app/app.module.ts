2cbb6f254848fd3c1c50d4efbe6bca8bbe663827
Andres Felipe Reyes Montes <anreyes@personalsoft.com.co>
2019-07-15 17:05:22 -0500

    Cambios en cliente de Angular

--- angular/clientes-app/src/app/app.module.ts
+++ angular/clientes-app/src/app/app.module.ts
@@ -6,15 +6,24 @@ import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { ClienteService } from './clientes/cliente.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path:'', redirectTo:'/clientes', pathMatch:'full'},
+  {path:'directivas', component:DirectivaComponent},
+  {path:'clientes', component:ClientesComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent, FooterComponent, DirectivaComponent, ClientesComponent, ClienteComponent
+    AppComponent, HeaderComponent, FooterComponent, DirectivaComponent, ClientesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, { enableTracing: true })
   ],
-  providers: [],
+  providers: [ ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular/clientes-app/src/app/clientes/clientes.component.ts
+++ angular/clientes-app/src/app/clientes/clientes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+
+
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
@@ -7,22 +10,17 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] = [
-    {id: 1, nombre:'Sandra', apellido:'Mendieta',email:'sandra@mail.com.co', createAt:'2019-05-30'},
-    {id: 2, nombre:'Maritza', apellido:'Silva',email:'maritza78@mail.com.co', createAt:'2019-05-29'},
-    {id: 3, nombre:'Natalia', apellido:'Ramirez',email:'natis@hotmail.com', createAt:'2019-05-15'},
-    {id: 4, nombre:'Diana', apellido:'Silva',email:'diana@mail.com.co', createAt:'2019-05-12'},
-    {id: 5, nombre:'Paola', apellido:'Perez',email:'paola.perez@mail.com.co', createAt:'2019-05-16'},
-    {id: 6, nombre:'Juan', apellido:'Marin',email:'juan@mail.com.co', createAt:'2019-05-01'},
-    {id: 7, nombre:'Carlos', apellido:'Rincon',email:'carlosmar@gmail.com', createAt:'2019-05-28'},
-    {id: 8, nombre:'Monica', apellido:'Naranjo',email:'naram@mail.com.co', createAt:'2019-05-20'},
-    {id: 9, nombre:'Dahiana', apellido:'Lopez',email:'dalopez@mail.com.co', createAt:'2019-05-05'},
-    {id: 10, nombre:'Astrid', apellido:'Pereira',email:'asper@mail.com.co', createAt:'2019-05-23'}
-  ];
+  clientes: Cliente[];
 
-  constructor() { }
+  constructor(private clienteService: ClienteService) { }
 
   ngOnInit() {
+    this.clienteService.getClientes().subscribe(
+      /*function (clientes) {
+        this.clientes = clientes
+      }*/
+      clientes => this.clientes = clientes
+    );
   }
 
 }
