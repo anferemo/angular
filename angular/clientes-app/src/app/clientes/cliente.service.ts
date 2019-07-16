74e651fcfabd915cbed690416baf4e8d23bc2c25
Andres Felipe Reyes Montes <anreyes@personalsoft.com.co>
2019-07-16 10:30:45 -0500

    Implementacion Inicial Llamado a Servicios de SpringBoot

--- angular/clientes-app/src/app/clientes/cliente.service.ts
+++ angular/clientes-app/src/app/clientes/cliente.service.ts
@@ -2,15 +2,23 @@ import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
 import {CLIENTES} from './clientes.json';
 import {of, Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';  //Para mapear el Request
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
-
-  constructor() { }
+  private urlEndpouint:string = 'http://localhost:8083/api/clientes';
+  constructor(private http:HttpClient) { }
 
   getClientes(): Observable<Cliente[]>{
-    return of(CLIENTES);
+    //return of(CLIENTES);
+    //Forma sencilla (Casteo al Objeto)
+    return this.http.get<Cliente[]>(this.urlEndpouint);
+    //Otra forma (Mapeo al request)
+    /*return this.http.get(this.urlEndpouint).pipe(
+      map(response => response as Cliente[] ) //Equivalente a map(function(response){return response as Cliente[]});
+    );*/
   }
 }
